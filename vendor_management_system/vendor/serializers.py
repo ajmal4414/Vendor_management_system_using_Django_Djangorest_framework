e8ddd15a6624301d5573884ed0d0773a1411304a
ajmal4414 <122264907+ajmal4414@users.noreply.github.com>
2023-12-11 20:36:47 +0530

    Update serializers.py

--- vendor_management_system/vendor/serializers.py
+++ vendor_management_system/vendor/serializers.py
@@ -3,16 +3,6 @@ from .models import Vendor
 
 from django.contrib.auth.models import User
 
-# class UserSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model=User
-#         fields=['username','password']
-#         def create(self,validated_data):
-
-#             user=User.object.create(username=validated_data['username'])
-#             user.set_password[validated_data['password']]
-#             user.save()
-#             return user
 
 
 class UserSerializer(serializers.ModelSerializer):
